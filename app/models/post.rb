6cee0ab403b1bea633c42016f031a25cb7fe1f75
Lianzi <lianziliu.sugar@gmail.com>
2022-12-15 17:07:49 -0500

    commit tree 2edc96dbfa5433fa18c340cf380cb8d29f52a3fa

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction','Non-Fiction'] }
+    validate :clickbait
+
+    # vaild_content={
+    #     "Won't Believe",
+    #     "Secret",
+    #     "Top [number]",
+    #     "Guess"
+    # }
+
+    def clickbait
+        if title == "True Facts"
+            errors.add :title, message: "There is an error!"
+        end
+    end
 end
